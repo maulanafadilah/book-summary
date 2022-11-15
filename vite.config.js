f5bd25527359d2c07487b88b594493b524e5df17
maulanafadilah <maulanafadilah@student.telkomuniversity.ac.id>
2022-11-15 15:40:25 +0700

    vite-config-edit-2

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            buildDirectory: '../../public/build',
+            // input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['/../../public_html/book_summary/build/resources/css/app.css', '/../../public_html/book_summary/build/resources/js/app.js'],
+            // buildDirectory: '../../public/build',
             refresh: true,
         }),
     ],
